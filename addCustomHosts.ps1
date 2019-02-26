574b3ac832c35e898f7147d5f1e7a34d64ef155c
Jussi Jaurola <jjaurola@gmail.com>
2019-02-26 08:27:59 +0200

    Create addCustomHosts.ps1

---
+++ addCustomHosts.ps1
@@ -0,0 +1,58 @@
+### usage: ./addCustomHosts.ps1 -vip 192.168.1.198 -username admin [ -domain local ] -hostfile 'hosts.txt'
+
+### Add custom host mappings from file - Jussi Jaurola <jussi@cohesity.com>
+###
+### File content should be like thisV:
+### 
+### "IPAddress", "Hostname"
+### 10.10.10.10,someserver
+### 192.168.12.13,jussi
+###
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter()][string]$domain = 'local',
+    [Parameter(Mandatory = $True)][string]$hostfile
+)
+
+### source the cohesity-api helper code
+. ./cohesity-api
+
+### authenticate
+apiauth -vip $vip -username $username -domain $domain
+
+# Get custom hosts from file
+try {
+    $hosts = Import-Csv $hostfile
+} catch {
+    write-host "Cannot open hostfile $hostfile" -ForegroundColor Yellow
+    exit
+}
+
+$hostData = @()
+foreach ($mapping in $hosts) 
+{
+    $domains = @()
+    $domains += $mapping.Hostname
+    $domainName = $mapping.Hostname
+    write-host "Adding custome mapping for $domainName"
+
+    $hostData += @{
+        "ip" = $mapping.IPAddress;
+        "domainName" = $domains
+    }
+}
+
+if ($hostData)
+{
+    write-host "Hostit"    
+    $hostsData = @{
+        "hosts" = $hostData
+    }
+
+    $hostsData | ConvertTo-Json -Depth 3
+    api put /nexus/cluster/upload_hosts_file $hostsData
+}
