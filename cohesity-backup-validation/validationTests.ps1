318a169c2f88e1b90685894b5699266c5ae0215c
Jussi Jaurola <jjaurola@gmail.com>
2019-10-02 14:29:18 +0300

    Update validationTests.ps1

--- cohesity-backup-validation/validationTests.ps1
+++ cohesity-backup-validation/validationTests.ps1
@@ -2,55 +2,40 @@
 ### Helper tool to build backup validation testing for VMs - Jussi Jaurola <jussi@cohesity.com>
 ###
 
-
 param (
-    #### Path to the environment JSON file for Cohesity and vCenter
-    [Parameter(Mandatory=$true)]
-    [ValidateScript({Test-Path $_})]
-    [String]$EnvironmentFile,
-    
-    #### Path to the configuration JSON file for applications
-    [Parameter(Mandatory=$true)]
-    [ValidateScript({Test-Path $_})]
-    [String]$ConfigFile,
-    
-    ### Path to the folder containing XML credential files for this buildrun
-    [Parameter(Mandatory=$true)]
-    [ValidateScript({Test-Path $_})]
-    [String]$IdentityPath
+    [Parameter(Mandatory=$true)][ValidateScript({Test-Path $_})][String]$EnvironmentFile,
+    [Parameter(Mandatory=$true)][ValidateScript({Test-Path $_})][String]$ConfigFile
 )
 
-### Get  configuration from config.json
+### Get  configuration from config.json for future reference use
 task getConfig {
     $script:Environment = Get-Content -Path $EnvironmentFile | ConvertFrom-Json
     $script:Config = Get-Content -Path $ConfigFile | ConvertFrom-Json
-    if ($IdentityPath.Substring($IdentityPath.Length - 1) -ne '\') {
-        $script:IdentityPath += '\'
-    }
 }
 
 ### Connect to Cohesity cluster
 task connectCohesity {
-    Write-Host "Importing Credential file: $($IdentityPath + $Environment.cohesityCred)" -ForegroundColor Yellow
-    $Credential = Import-Clixml -Path ($IdentityPath + $Environment.cohesityCred)
+    Write-Host "Importing Credential file: $($Environment.cohesityCred)" -ForegroundColor Yellow
+    Write-Host "Connecting to Cohesity Cluster $($Environment.cohesityCluster)" -ForegroundColor Yellow
+    $Credential = Import-Clixml -Path ($Environment.cohesityCred)
     try {
         Connect-CohesityCluster -Server $Environment.cohesityCluster -Credential $Credential
         Write-Host "Connected to Cohesity Cluster $($Environment.cohesityCluster)" -ForegroundColor Yellow
     } catch {
-        write-host "Cannot connect to Cohesity cluster $Environment.cohesityCluster" -ForegroundColor Yellow
+        write-host "Cannot connect to Cohesity cluster $($Environment.cohesityCluster)" -ForegroundColor Yellow
         exit
     }
 }
 
 ### Connect to VMware vCenter
 task connectVMware {
-    Write-Host "Getting credentials from credential file $($IdentityPath + $Environment.vmwareCred)" -ForegroundColor Yellow
-    $Credential = Import-Clixml -Path ($IdentityPath + $Environment.vmwareCred)
+    Write-Host "Getting credentials from credential file $($Environment.vmwareCred)" -ForegroundColor Yellow
+    $Credential = Import-Clixml -Path ($Environment.vmwareCred)
     try {
         Connect-VIServer -Server $Environment.vmwareServer -Credential $Credential
         Write-Host "Connected to VMware vCenter $($global:DefaultVIServer.Name)" -ForegroundColor Yellow
     } catch {
-        write-host "Cannot connect to VMware vCenter $Environment.vmwareServer" -ForegroundColor Yellow
+        write-host "Cannot connect to VMware vCenter $($Environment.vmwareServer)" -ForegroundColor Yellow
         exit
     }
 }
@@ -58,18 +43,29 @@ task connectVMware {
 ### Create a clone task for virtual machine(s)
 task createCloneTask {
 
+    if (!$Environment.vmwareServer) { throw "Clone task failed. VMware Server is not defined in environment json-file"  }
+    if (!$Environment.vmwareResourcePool) { throw "Clone task failed. VMware Resource Pool is not defined in config json-file" }
+
     # Get vmware source ID for resource pool
     $vmwareSource = Get-CohesityProtectionSourceObject -Environments kVMware | Where-Object Name -eq $Environment.vmwareServer 
     $vmwareResourcePool = Get-CohesityProtectionSourceObject -Environments kVMware | Where-Object ParentId -eq $($vmwareSource.Id) | Where-Object Name -eq $Environment.vmwareResourcePool 
 
-    # Uses a null array of Mount IDs that will be used to track the request process
+    if (!$vmwareSource) {
+        throw "Couldnt find Protection Source $($Environment.vmwareServer). Failing tests. Please check!"
+    }
+
     [Array]$Script:CloneArray = $null
     foreach ($VM in $Config.virtualMachines) {  
-        $backupJob =  Find-CohesityObjectsForRestore -Search $($VM.name) -environments kVMware | Where-Object JobName -eq $($VM.backupJobName) 
+        if (!$VM.backupJobName) { throw "Clone task failed. Bckupjob is not defined for VM $($VM.name)" }
+        ### if multiple objects found with same object name, use first
+        $backupJob =  Find-CohesityObjectsForRestore -Search $($VM.name) -environments kVMware | Where-Object JobName -eq $($VM.backupJobName) | Select-Object -first 1 
         $cloneVM = Get-CohesityVMwareVM -name $VM.name 
+        if (!$cloneVM) {
+            Write-Host "Couldnt find VM $($VM.name). Failing tests. Please check!" -ForegroundColor Red
+            exit
+        }
         $cloneTask = Copy-CohesityVMwareVM -TaskName "BTest_$($VM.name)" -PoweredOn:$true -DisableNetwork:$false -Jobid $($backupJob.JobId) -SourceId $($cloneVM.id) -TargetViewName "BTest_$($VM.name)" -VmNamePrefix "BTest_" -ResourcePoolId $($vmwareResourcePool.id) -NewParent $($vmwareSource.Id)
         Write-Host "Created cloneTask $($cloneTask.Id) for VM $($VM.name)" -ForegroundColor Yellow
-
         $Script:CloneArray += $cloneTask
     }
 }
@@ -79,7 +75,7 @@ task checkCloneTask {
         while ($true) {
             $validateTask = (Get-CohesityRestoreTask -Id $Clone.Id).Status
             $validatePowerOn = (Get-VM -Name $Clone.Name -ErrorAction:SilentlyContinue).PowerState
-
+            Start-Sleep 10
             Write-Host "$($Clone.Name) clone status is $validateTask and Power Status is $ValidatePowerOn" -ForegroundColor Yellow
             if ($validateTask -eq 'kFinished' -and $validatePowerOn -eq 'PoweredOn') {
                 break
@@ -110,31 +106,35 @@ task checkVmwareTools {
 }
 
 task checkPSScriptExecution {
+    $i = 0
     foreach ($Clone in $CloneArray) {
-        $count = 1
+        $vmCredentials = Import-Clixml -Path ($Config.virtualMachines[$i].guestCred)
+        $loopCount = 1
         while ($true) {
-
-            Write-Host "Run ($count): Script test on $($Clone.name)" -ForegroundColor Yellow
-            $splat = @{
+            Write-Host "Run try ($loopCount)/5: Script test on $($Clone.name)" -ForegroundColor Yellow
+            $vmscript = @{
                 ScriptText      = 'hostname'
                 ScriptType      = 'PowerShell'
-                VM              = $Clone.name
+                VM              = $Clonename
                 GuestCredential = $vmCredentials
             }
             try {
-                $results = Invoke-VMScript @splat -ErrorAction Stop
+                $results = Invoke-VMScript @vmscript -ErrorAction Stop
                 Write-Host "checkPSScriptExecution status $results" -ForegroundColor Yellow
                 break
             } catch { }
             
-            $count++
-            Sleep -Seconds 5
+            $loopCount++
+            Sleep -Seconds 30
             
             if ($LoopCount -gt 5) {
-                throw "Could not execute script on: $($Clone.Name)..."
+                Write-Host "Could not execute script on $($Clone.Name), failing tests!" -ForegroundColor Red
+                Exit
             }
         }
+        $i++
     }
+
 }
 
 ### Config network for cloned VMs
@@ -153,28 +153,39 @@ task configVMNetwork {
 task configVMNetworkIP {
     $i = 0
     foreach ($Clone in $CloneArray) {
-        Write-Host "$($Clone.Name): Importing credential file $($IdentityPath + $($Config.virtualMachines[$i].guestCred))" -ForegroundColor Yellow
-        $vmCredentials = Import-Clixml -Path ($IdentityPath + $($Config.virtualMachines[$i].guestCred))
-        
-        $TestInterfaceMAC = ((Get-NetworkAdapter -VM $($Clone.Name) | Select-Object -first 1).MacAddress).ToLower() -replace ":","-"
-        $splat = @{
-            ScriptText      = 'Get-NetAdapter | where {($_.MacAddress).ToLower() -eq "' + $TestInterfaceMAC + '"} | Remove-NetRoute -Confirm:$false -ErrorAction SilentlyContinue;`
-                               Get-NetAdapter | where {($_.MacAddress).ToLower() -eq "' + $TestInterfaceMAC + '"} | Get-NetIPAddress | Remove-NetIPAddress -confirm:$false;`
-                               Get-NetAdapter | where {($_.MacAddress).ToLower() -eq "' + $TestInterfaceMAC + '"} | `
-                               New-NetIPAddress -IPAddress ' + $Config.virtualMachines[$i].testIp + ' -PrefixLength ' + $Config.virtualMachines[$i].testSubnet + `
-                               ' -DefaultGateway ' + $Config.virtualMachines[$i].testGateway
-            ScriptType      = 'PowerShell'
-            VM              = $Clone.Name
-            GuestCredential = $vmCredentials
+        Write-Host "$($Clone.Name): Importing credential file $($Config.virtualMachines[$i].guestCred))" -ForegroundColor Yellow
+        $vmCredentials = Import-Clixml -Path ($Config.virtualMachines[$i].guestCred)
+
+        if ($Config.virtualMachines[$i].guestOS -eq 'Windows') {
+            $TestInterfaceMAC = ((Get-NetworkAdapter -VM $($Clone.Name) | Select-Object -first 1).MacAddress).ToLower() -replace ":","-"
+            $splat = @{
+                ScriptText      = 'Get-NetAdapter | where {($_.MacAddress).ToLower() -eq "' + $TestInterfaceMAC + '"} | Remove-NetRoute -Confirm:$false -ErrorAction SilentlyContinue;`
+                                Get-NetAdapter | where {($_.MacAddress).ToLower() -eq "' + $TestInterfaceMAC + '"} | Get-NetIPAddress | Remove-NetIPAddress -confirm:$false;`
+                                Get-NetAdapter | where {($_.MacAddress).ToLower() -eq "' + $TestInterfaceMAC + '"} | `
+                                New-NetIPAddress -IPAddress ' + $Config.virtualMachines[$i].testIp + ' -PrefixLength ' + $Config.virtualMachines[$i].testSubnet + `
+                                ' -DefaultGateway ' + $Config.virtualMachines[$i].testGateway
+                ScriptType      = 'PowerShell'
+                VM              = $Clone.Name
+                GuestCredential = $vmCredentials
+            }
+            $output = Invoke-VMScript @splat -ErrorAction Stop
+            $run = @{
+                ScriptText      = '(Get-NetAdapter| where {($_.MacAddress).ToLower() -eq "' + $TestInterfaceMAC + '"} | Get-NetIPAddress -AddressFamily IPv4).IPAddress'
+                ScriptType      = 'PowerShell'
+                VM              = $Clone.Name
+                GuestCredential = $vmCredentials
+            }
+        } 
+
+        if ($Config.guestOS -eq 'Linux')  {
+            
         }
-        $output = Invoke-VMScript @splat -ErrorAction Stop
-        $splat = @{
-            ScriptText      = '(Get-NetAdapter| where {($_.MacAddress).ToLower() -eq "' + $TestInterfaceMAC + '"} | Get-NetIPAddress -AddressFamily IPv4).IPAddress'
-            ScriptType      = 'PowerShell'
-            VM              = $Clone.Name
-            GuestCredential = $vmCredentials
+
+        if (!run) { 
+            throw "$($Clone.Name): Network IP change to $($Config.virtualMachines[$i].testIp) failed. GuestOS $($Config.virtualMachines[$i].guestOS) is not supported (Windows/Linux). Cloned VMs remain cloned status, manual cleanup might needed!" 
         }
-        $output = Invoke-VMScript @splat -ErrorAction Stop
+
+        $output = Invoke-VMScript @run -ErrorAction Stop
         $new_ip = $output.ScriptOutput -replace "`r`n", ""
         if ( $new_ip -eq $Config.virtualMachines[$i].testIp ) {
             Write-Host "$($Clone.Name): Network IP changed to $($new_ip)" -ForegroundColor Yellow
@@ -191,7 +202,7 @@ task validationTests {
     $i = 0
     foreach ($Clone in $CloneArray) {
         Write-Host "$($Clone.Name): Running tests $($Config.virtualMachines[$i].tasks)" -ForegroundColor Yellow
-        $vmCredentials = Import-Clixml -Path ($IdentityPath + $($Config.virtualMachines[$i].guestCred))
+        $vmCredentials = Import-Clixml -Path ($Config.virtualMachines[$i].guestCred)
         Invoke-Build -File .\validationTests.ps1 -Task $Config.virtualMachines[$i].tasks -Config $Config.virtualMachines[$i] -GuestCredential $vmCredentials
         Write-Host "$($Clone.Name): Testing complete" -ForegroundColor Yellow
         $i++
